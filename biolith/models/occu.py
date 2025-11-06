fix observation masking logic

biolith/models/occu.py
@@ -8,6 +8,7 @@ import numpyro
 import numpyro.distributions as dist
 
 from biolith.regression import AbstractRegression, LinearRegression
+from biolith.utils.modeling import mask_missing_obs
 from biolith.utils.spatial import sample_spatial_effects, simulate_spatial_effects
 
 
@@ -207,19 +208,11 @@ def occu(
                 * (1 - (1 - z) * prob_fp_unoccupied),
             )
 
-            if obs is not None:
-                with numpyro.handlers.mask(mask=jnp.isfinite(obs)):
-                    numpyro.sample(
-                        f"y",
-                        dist.Bernoulli(prob_detection_fp),  # type: ignore
-                        obs=jnp.nan_to_num(obs),
-                        infer={"enumerate": "parallel"},
-                    )
-            else:
+            with mask_missing_obs(obs):
                 numpyro.sample(
                     f"y",
                     dist.Bernoulli(prob_detection_fp),  # type: ignore
-                    infer={"enumerate": "parallel"},
+                    obs=obs,
                 )
 
 

biolith/models/occu_cop.py
@@ -8,6 +8,7 @@ import numpyro
 import numpyro.distributions as dist
 
 from biolith.regression import AbstractRegression, LinearRegression
+from biolith.utils.modeling import mask_missing_obs
 from biolith.utils.spatial import sample_spatial_effects, simulate_spatial_effects
 
 
@@ -215,16 +216,11 @@ def occu_cop(
             )
             l_det = z * rate_detection + (1 - z) * rate_fp_unoccupied + rate_fp_constant
 
-            if obs is not None:
-                with numpyro.handlers.mask(mask=jnp.isfinite(obs)):
-                    numpyro.sample(
-                        f"y",
-                        dist.Poisson(jnp.nan_to_num(session_duration * l_det)),
-                        obs=jnp.nan_to_num(obs),
-                    )
-            else:
+            with mask_missing_obs(obs):
                 numpyro.sample(
-                    f"y", dist.Poisson(jnp.nan_to_num(session_duration * l_det))
+                    f"y",
+                    dist.Poisson(session_duration * l_det),
+                    obs=obs,
                 )
 
 

biolith/models/occu_cs.py
@@ -8,6 +8,7 @@ import numpyro
 import numpyro.distributions as dist
 
 from biolith.regression import AbstractRegression, LinearRegression
+from biolith.utils.modeling import mask_missing_obs
 from biolith.utils.spatial import sample_spatial_effects, simulate_spatial_effects
 
 
@@ -184,19 +185,13 @@ def occu_cs(
                 infer={"enumerate": "parallel"},
             )
 
-            if obs is not None:
-                with numpyro.handlers.mask(mask=jnp.isfinite(obs)):
-                    numpyro.sample(
-                        "s",
-                        dist.Normal(
-                            (1 - f) * mu0 + f * mu1, (1 - f) * sigma0 + f * sigma1  # type: ignore
-                        ),  # type: ignore
-                        obs=jnp.nan_to_num(obs),
-                    )
-            else:
+            with mask_missing_obs(obs):
                 numpyro.sample(
                     "s",
-                    dist.Normal((1 - f) * mu0 + f * mu1, (1 - f) * sigma0 + f * sigma1),  # type: ignore
+                    dist.Normal(
+                        (1 - f) * mu0 + f * mu1, (1 - f) * sigma0 + f * sigma1  # type: ignore
+                    ),  # type: ignore
+                    obs=obs,
                 )
 
 

biolith/models/occu_rn.py
@@ -9,6 +9,7 @@ import numpyro.distributions as dist
 
 from biolith.regression import AbstractRegression, LinearRegression
 from biolith.utils.distributions import RightTruncatedPoisson
+from biolith.utils.modeling import mask_missing_obs
 from biolith.utils.spatial import sample_spatial_effects, simulate_spatial_effects
 
 
@@ -181,19 +182,11 @@ def occu_rn(
             )
             p_it = 1.0 - (1.0 - r_it) ** N_i[None, :]  # type: ignore
 
-            if obs is not None:
-                with numpyro.handlers.mask(mask=jnp.isfinite(obs)):
-                    numpyro.sample(
-                        "y",
-                        dist.Bernoulli(1 - (1 - p_it) * (1 - prob_fp_constant)),  # type: ignore
-                        obs=jnp.nan_to_num(obs),
-                        infer={"enumerate": "parallel"},
-                    )
-            else:
+            with mask_missing_obs(obs):
                 numpyro.sample(
                     "y",
                     dist.Bernoulli(1 - (1 - p_it) * (1 - prob_fp_constant)),  # type: ignore
-                    infer={"enumerate": "parallel"},
+                    obs=obs,
                 )
 
 